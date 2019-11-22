45f16e1d163fea536f37ce5d310b27ded5b3049b
shoya4000 <mikevoorthuyzen@hotmail.com>
2019-11-22 14:55:19 -0800

    inpu

--- python-to-postgresql.py
+++ python-to-postgresql.py
@@ -2,8 +2,10 @@
 
 import psycopg2
 
+user = input("username: ")
+password = getpass.getpass(prompt='database password: ')
 conn = psycopg2.connect(host="studentdb1.csc.uvic.ca",
-                        database="the_homies", user="shoya", password="V00730770")
+                        database="the_homies", user=user, password=password)
 cur = conn.cursor()
 
 cur.execute('SELECT * FROM user_acc')
