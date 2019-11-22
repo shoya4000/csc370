36f61d92e410f7b0e0f904cb57dba37326da3408
shoya4000 <mikevoorthuyzen@hotmail.com>
2019-11-22 15:05:16 -0800

    marten sucks

--- python-to-postgresql.py
+++ python-to-postgresql.py
@@ -8,7 +8,7 @@ password = getpass.getpass(prompt='database password: ')
 conn = psycopg2.connect(host="studentdb1.csc.uvic.ca",
                         database="the_homies", user=username, password=password)
 cur = conn.cursor()
-while(True)
+while(True):
     psql = raw_input(">")
     cur.execute(psql)
     print(cur.fetchone())
