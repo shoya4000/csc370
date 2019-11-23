afc46d025cd735d3b253363ca232bf58ba3ac088
Sho Ya <mikevoorthuyzen@hotmail.com>
2019-11-23 00:40:10 -0800

    try option for \d

--- python-to-postgresql.py
+++ python-to-postgresql.py
@@ -2,13 +2,16 @@
 
 import psycopg2
 import getpass
+import subprocess
 
 username = raw_input("username: ")
 password = getpass.getpass(prompt='database password: ')
 conn = psycopg2.connect(host="studentdb1.csc.uvic.ca",
                         database="the_homies", user=username, password=password)
 cur = conn.cursor()
-cur.execute("\d")
+res = subprocess.run('psql -c "\d+ user_acc" test postgres',
+                     stdout=subprocess.PIPE)
+print(res.stdout.decode(sys.stdout.encoding))
 while(True):
     raw = raw_input(">")
     command = cur.mogrify(raw)
