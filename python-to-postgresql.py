93f1bcfe91818babcf85f1305b9dcb947edd4be0
Sho Ya <mikevoorthuyzen@hotmail.com>
2019-11-23 00:52:32 -0800

    remove subprocess

--- python-to-postgresql.py
+++ python-to-postgresql.py
@@ -9,9 +9,7 @@ password = getpass.getpass(prompt='database password: ')
 conn = psycopg2.connect(host="studentdb1.csc.uvic.ca",
                         database="the_homies", user=username, password=password)
 cur = conn.cursor()
-res = subprocess.call('psql -c "\d+ user_acc" test postgres',
-                      stdout=subprocess.PIPE)
-print(res.stdout.decode(sys.stdout.encoding))
+
 while(True):
     raw = raw_input(">")
     if (str(raw) == '\d'):
