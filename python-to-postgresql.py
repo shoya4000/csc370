3974440a9a75c1e65fa4235882fb0532ae79de64
Sho Ya <mikevoorthuyzen@hotmail.com>
2019-11-23 00:15:26 -0800

    add mogrify

--- python-to-postgresql.py
+++ python-to-postgresql.py
@@ -9,9 +9,10 @@ conn = psycopg2.connect(host="studentdb1.csc.uvic.ca",
                         database="the_homies", user=username, password=password)
 cur = conn.cursor()
 while(True):
-    psql = raw_input(">")
+    raw = raw_input(">")
+    command = cur.mogrify(raw)
     try:
-        cur.execute(psql)
+        cur.execute(command)
         result = cur.fetchall()
         for line in result:
             print(line)
