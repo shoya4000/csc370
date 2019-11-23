1a3c0a88b7dd03d862e180d0b99437c692ee23e9
Sho Ya <mikevoorthuyzen@hotmail.com>
2019-11-23 02:58:47 -0800

    clean up2

--- python-to-postgresql.py
+++ python-to-postgresql.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python
-from __future__ import print_function
 import psycopg2
 import getpass
 
 username = raw_input("username: ")
 password = getpass.getpass(prompt='database password: ')
+# establish connection
 conn = psycopg2.connect(host="studentdb1.csc.uvic.ca",
                         database="the_homies", user=username, password=password)
 cur = conn.cursor()
 
 while(True):
     raw = raw_input(">")
+    # handle \d for quick view of schema
     if (str(raw) == '\d'):
         raw = '''SELECT n.nspname as "Schema",
   					c.relname as "Name",
@@ -24,34 +25,44 @@ while(True):
       					AND n.nspname !~ '^pg_toast'
   					AND pg_catalog.pg_table_is_visible(c.oid)
 				ORDER BY 1,2;'''
+    # handle \q to quit
     elif (str(raw) == '\q'):
         quit()
+    # convert command into proper SQL format
     command = cur.mogrify(raw)
     try:
         cur.execute(command)
 
-        table = []
-
-        headers = []
-        for header in cur.description:
-            headers.append(header.name)
-        table.append(headers)
-
-        result = cur.fetchall()
-        for row in result:
-            string_row = []
-            for item in row:
-                string_row.append(str(item))
-            table.append(string_row)
-
-        row_format = ""
-        for i in range(len(headers)):
-            row_format += "{:<%d}" % (len(max([row[i]
-                                               for row in table], key=len)) + 1)
-        for row in table:
-            print(row_format.format(*row))
-
+        formatAndPrintResultTable(cur)
+        # commit the change to the database
         conn.commit()
     except psycopg2.Error as e:
         print(e.pgerror)
+        # rollback error caused
         conn.rollback()
+
+
+def formatAndPrintResultTable(cur):
+    # instantiate table to fill for printing
+    table = []
+    # gather headers
+    headers = []
+    for header in cur.description:
+        headers.append(header.name)
+    table.append(headers)
+    # gather rows
+    result = cur.fetchall()
+    for row in result:
+        string_row = []
+        for item in row:
+            string_row.append(str(item))
+        table.append(string_row)
+    # define column width (longest item + 1)
+    row_format = ""
+    for i in range(len(headers)):
+        row_format += "{:<%d}" % (len(max([row[i]
+                                           for row in table], key=len)) + 1)
+    # print the result table
+    for row in table:
+        print(row_format.format(*row))
+    # commit the change to the database
